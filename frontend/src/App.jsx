7a0f6fbca4caa5680cf6e9586858bc433619a8c8
myselfaryan <aaryansharma.1210@gmail.com>
2025-01-08 19:22:50 +0530

    added pages and routes

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,12 +1,27 @@
 import React from 'react'
 import { Button } from "@/components/ui/button"
+import { BrowserRouter } from 'react-router-dom'
+import NewsArticles from './pages/NewsArticles'
+import Dashboard from './pages/Dashboard'
+import About from './pages/About'
+import Home from './pages/Home'
+import SignInForm from './auth/forms/SignInForm'
+import SignUpForm from './auth/forms/SignUpForm'
 
 function App() {
   return (
-    <div>
-      <div>App</div> 
-      <Button>Button</Button>
-    </div>
+    <BrowserRoute>
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="/auth" element={<AuthLayout />}>
+        <Route path="/sign-in" element={<SignInForm />} />
+        <Route path="/sign-up" element={<SignUpForm />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/dashboard" element={<Dashboard />} />
+        <Route path="/news" element={<NewsArticles  />} />
+      </Route>
+    </Routes>
+    </BrowserRoute >
   )
 }
 
